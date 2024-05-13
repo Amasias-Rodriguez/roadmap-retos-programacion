1edadb7e8d74e873a0ad00261d31e3492414c28d
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2024-05-12 21:30:56 -0600

    sintax javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/Amasias-Rodriguez.js
@@ -0,0 +1,17 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+/* https://developer.mozilla.org/es/docs/Web/JavaScript */
+
+var TextChain = "Some random text";
+
+const String = "Thi is a string"
+
+let Boolean = true;
+
+const Number = 23;
+
+let Decimal = 3.14;
+
+let Objetc = { name: Mike, age: 32, country: England};
+
+console.log("Hello World");
